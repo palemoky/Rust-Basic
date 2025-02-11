d50c5d4cc0ed122a5752508affb4630105fc1c1c
Palemoky <palemoky@gmail.com>
2025-02-11 10:25:22 +0800

    done quiz

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,16 +16,18 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard <T, V> {
+    pub grade: T,
     pub student_name: String,
-    pub student_age: u8,
+    pub student_age: V,
 }
 
-impl ReportCard {
-    pub fn print(&self) -> String {
+pub trait Printable {
+    fn print(&self) -> String;
+}
+
+impl<T: std::fmt::Display, V: std::fmt::Display> Printable for ReportCard <T, V> {
+    fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
     }
@@ -52,7 +54,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
